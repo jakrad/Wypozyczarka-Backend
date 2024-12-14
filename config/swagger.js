7fd282bf039b4fd92d33b2c814eeb2aa97282c77
Jakub Radwanski <jakradwanski@outlook.com>
2024-12-14 15:05:59 +0100

    Update Swagger configuration to use HTTPS for the development server

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/api',
+        url: 'https://localhost:3000/api',
         description: 'Development server',
       },
     ],
