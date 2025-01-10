5155863c5ad9f056cdd77fedb137e8e2a32a05c6
Jakub Radwanski <jakradwanski@outlook.com>
2025-01-10 17:25:44 +0100

    Add lastLogin field to User model for tracking user login timestamps

--- models/User.js
+++ models/User.js
@@ -1,52 +1,61 @@
 // models/User.js
-const { DataTypes } = require('sequelize');
-const sequelize = require('./sequelize');
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const { DataTypes } = require("sequelize");
+const sequelize = require("./sequelize");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 100], // min 8 znaków
+      },
+    },
+    profileImage: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: /^\+?[\d\s-]{8,}$/, // format numeru telefonu
+      },
+    },
+    lastLogin: {
+      type: DataTypes.BIGINT, // Stores timestamp in milliseconds
+      allowNull: true,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      isEmail: true
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [8, 100] // min 8 znaków
-    }
-  },
-  profileImage: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phoneNumber: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      is: /^\+?[\d\s-]{8,}$/ // format numeru telefonu
-    }
+  {
+    tableName: "user",
+    timestamps: true,
+    underscored: true,
   }
-}, {
-  tableName: 'user',
-  timestamps: true,
-  underscored: true
-});
+);
 
 // Relacje w index.js
 
